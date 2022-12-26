113b833a448d4cd1fb072b44938b1ac4b3b76814
Willy <leaguematter@likelion.org>
2022-12-26 10:52:46 +0900

    modifying

--- weberry_be/src/main/java/com/weberry/backend/controller/AuthController.java
+++ weberry_be/src/main/java/com/weberry/backend/controller/AuthController.java
@@ -3,7 +3,10 @@ package com.weberry.backend.controller;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
@@ -48,33 +52,21 @@ public class AuthController {
 	}
 	
 	@PostMapping(path="/sign-up")
-	public String signUp(@RequestBody ObjectNode infos, RedirectAttributes attribute) throws JsonProcessingException, IllegalArgumentException {
+	public ModelAndView signUp(@RequestBody ObjectNode infos, HttpServletRequest request) throws JsonProcessingException, IllegalArgumentException {
 		ObjectMapper mapper = new ObjectMapper();
 		User.Request userInfo = mapper.treeToValue(infos.get("userInfo"), User.Request.class);
 		Farm farmInfo = mapper.treeToValue(infos.get("farmInfo"), Farm.class);
-		attribute.addFlashAttribute("user", userService.createUser(userInfo, farmInfo));
+		ModelAndView modelAndView = new ModelAndView("redirect:sign-in");
+		modelAndView.addObject("user", userService.createUser(userInfo, farmInfo));
+		System.out.println("sign-up: " + modelAndView);
+		request.setAttribute(View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.TEMPORARY_REDIRECT);
 		
-		return "redirect:/auth/sign-in";
+		return modelAndView;
 	}
-//	
-//	@PostMapping(path="/sign-up")
-//	public User.SignIn signUp(@RequestBody ObjectNode infos) throws JsonProcessingException, IllegalArgumentException {
-//		ObjectMapper mapper = new ObjectMapper();
-//		User.Request userInfo = mapper.treeToValue(infos.get("userInfo"), User.Request.class);
-//		Farm farmInfo = mapper.treeToValue(infos.get("farmInfo"), Farm.class);
-//		
-//		return userService.createUser(userInfo, farmInfo);
-//	}
-//	
-//	@PostMapping(path="/sign-in")
-//	public String signIn(@RequestBody User user) {
-//		
-//		return userService.signIn(user);
-//	}
 	
 	@PostMapping(path="/sign-in")
-	public String signIn(RedirectAttributes attribute) {
-		System.out.println(attribute);
+	public String signIn(ModelAndView modelAndView) {
+		System.out.println("sign-in: " + modelAndView);
 		
 		return userService.signIn((User) modelAndView.getModel().get("user"));
 	}
