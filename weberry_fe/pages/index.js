625e23daa90a62a48dbacbd5f8ab00fdbe0c190b
naka97 <naka97@kakao.com>
2022-12-26 15:22:29 +0900

    frontend setting

---
+++ weberry_fe/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import main from '../components/main/main'
+import styles from '../styles/Home.module.css'
+import Main from '../components/main/main'
+export default function Home() {
+  return (
+    <Main />
+  )
+}
