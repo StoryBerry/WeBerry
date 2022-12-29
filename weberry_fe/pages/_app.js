f7b75f8773d1a52fb27b0a6f07d077604ab24f7f
CHACHA <naka97@kakao.com>
2022-12-29 19:12:24 +0900

    designing main layout

--- weberry_fe/pages/_app.js
+++ weberry_fe/pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import Header from '../components/Layout/Header'
+import Footer from '../components/Layout/Footer'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header/>
+      <div className='font-extralight'>
+      <Component {...pageProps} />
+      </div>
+      <Footer />
+    </>
+  )
+
 }
 
+/**mx-auto max-w-5xl px-4 sm:px-6 flex  items-center justify-between  */
 export default MyApp
+
--- weberry_fe/tailwind.config.js
+++ weberry_fe/tailwind.config.js
@@ -4,8 +4,29 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
-    extend: {},
+    fill: theme => ({
+      'red': theme('colors.red.500'),
+      'green': theme('colors.green.500'),
+      'blue': theme('colors.blue.500'),
+    }),
+
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'grey': "#d3d3d3",
+      'light_pink': "#ff8468",
+      'black_600' :"#4B5563",
+      'yello_100' : "#FDF6B2",
+    },
+    extend: {
+
+    },
+  },
+  variants: {
+    fill: ['hover', 'focus'],
   },
   plugins: [],
 }
