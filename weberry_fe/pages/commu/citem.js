297118cc6d480f0c226c5ed4494722ee5d09fb47
CHACHA <naka97@kakao.com>
2023-01-02 18:58:47 +0900

    main finish 230102
    Signed-off-by: CHACHA <naka97@kakao.com>

---
+++ weberry_fe/pages/commu/citem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import CommuItem from '../../components/Commu/CommuItem'
+import styles from '../../styles/Home.module.css'
+
+
+const citem = () => {
+  return (
+    <>
+    <div className={styles.main}>
+    <CommuItem />
+    </div>
+    </>
+    )
+}
+
+export default citem
