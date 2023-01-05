47d9f0cd6bc3335f13d151190c90d724d8d6e506
ONEHENA <biny159@gmail.com>
2023-01-05 16:13:26 +0900

    merging

---
+++ weberry_fe/pages/mmchat/index.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import Chat from '../../components/MMchat/chat'
+
+const index = () => {
+
+  return (
+    <div className='mt-20 px-4'>
+      <Chat />
+    </div>
+  )
+  
+}
+
+export default index
