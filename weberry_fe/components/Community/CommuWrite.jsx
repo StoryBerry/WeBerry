724141e44a468b12bfb0006ff7d0d5783eb0738d
chspower1 <chspower1@gmail.com>
2023-01-06 00:10:11 +0900

    feat: preview image

--- weberry_fe/components/Community/CommuWrite.jsx
+++ weberry_fe/components/Community/CommuWrite.jsx
@@ -10,7 +10,7 @@ const CommuWrite = (props) => {
   const [content, setContent] = useState("");
   const [images, setImage] = useState([]);
   const [signIn, setSignIn] = useState({});
-
+  const [imagePreview, setImagePreview] = useState("");
   const clickHandler = () => setToWritePost(!toWritePost);
   const contentHandler = (event) => setContent(event.target.value);
   const imageHandler = (event) => {
@@ -18,16 +18,15 @@ const CommuWrite = (props) => {
     console.log(event.target.files);
   };
   const buttonHandler = (event) => {
-    event.target.innerText === "작성"
-      ? writePost()
-      : setToWritePost(!toWritePost);
+    event.target.innerText === "작성" ? writePost() : setToWritePost(!toWritePost);
   };
   const writePost = () => {
+    console.log(content, signIn);
     const formData = new FormData();
     const body = { content: content, user: signIn };
     formData.set("request", body);
     formData.set("imageFiles", images[0]);
-    console.log("imageFiles: " + formData.get("imageFiles"));
+    console.dir("imageFiles: " + formData.get("imageFiles"));
     console.log(`content: ${content}`);
     console.log(`signIn: ${signIn}`);
     console.log(URL.createObjectURL(images[0]));
@@ -43,16 +42,19 @@ const CommuWrite = (props) => {
       headers: { Authorization: token.token },
     })
       .then((response) => response.json())
-      .then((data) => setSignIn({ ...JSON.parse(data["signIn"]) }))
+      .then((data) => setSignIn({ user: { ...JSON.parse(data["signIn"]) } }))
       .catch((err) => console.error(err));
   }, []);
-
+  useEffect(() => {
+    if (images && images.length > 0) {
+      const file = images[0];
+      console.log(URL.createObjectURL(file));
+      setImagePreview(URL.createObjectURL(file));
+    }
+  }, [avatar]);
   return (
     <>
-      <div
-        className="fixed bg-grey/80 z-10 w-full h-full mt-16 "
-        onClick={clickHandler}
-      />
+      <div className="fixed bg-grey/80 z-10 w-full h-full mt-16 " onClick={clickHandler} />
       {signIn["userid"] ? (
         <div className="fixed bg-white z-20 mt-24 w-4/5 left-9 content-center">
           <textarea
@@ -73,15 +75,18 @@ const CommuWrite = (props) => {
             multiple="multiple"
             accept="image/png, image/jpg"
           />
+          {imagePreview && <img src={imagePreview} />}
           <div className="buttons flex w-4/5 justify-around mx-8 my-4">
             <div
               className="button w-16 p-2 bg-water text-black font-semibold rounded-lg shadow-md"
-              onClick={buttonHandler}>
+              onClick={buttonHandler}
+            >
               작성
             </div>
             <div
               className="button w-16 p-2 bg-grey text-black font-semibold rounded-lg shadow-md"
-              onClick={buttonHandler}>
+              onClick={buttonHandler}
+            >
               취소
             </div>
           </div>
